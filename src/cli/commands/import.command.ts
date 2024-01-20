2205b25e51569ca8556b4d190b89679d44ce98f8
Nikita <tereshkonikita@mail.ru>
2024-01-20 15:02:00 +0300

    finish ImportCommand

--- src/cli/commands/import.command.ts
+++ src/cli/commands/import.command.ts
@@ -3,6 +3,7 @@
 */
 
 import { Command } from './command.interface.js';
+import { TSVFileReader } from '../../shared/libs/file-reader/tsv-file-reader.js';
 
 export class ImportCommand implements Command {
   public getName(): string {
@@ -10,6 +11,20 @@ export class ImportCommand implements Command {
   }
 
   public execute(...params: string[]): void {
-    // чтение
+    const [filename] = params;
+    const FileReader = new TSVFileReader(filename.trim());
+
+    try {
+      FileReader.read();
+      console.log(FileReader.toArray());
+    } catch (err) {
+
+      if (!(err instanceof Error)) {
+        throw err;
+      }
+
+      console.error(`Can't import data from file: ${filename}`);
+      console.error(`Details: ${err.message}`);
+    }
   }
 }
