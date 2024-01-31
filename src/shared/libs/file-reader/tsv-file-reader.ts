72de9e8500fdb1c3cd5dd3c0bffe3798c11db163
Nikita <tereshkonikita@mail.ru>
2024-01-31 23:19:59 +0300

    refactoring ImportCommand

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -3,20 +3,42 @@
 */
 
 import { FileReader } from './file-reader.interface.js';
-import { readFileSync } from 'node:fs';
-import { City, Housing, RentalOffer, UserType } from '../../types/index.js';
+import EventEmitter from 'node:events';
+import { createReadStream } from 'node:fs';
+//import { City, Housing, RentalOffer, UserType } from '../../types/index.js';
 
-export class TSVFileReader implements FileReader {
-  private rawData = '';
+const CHUNK_SIZE = 16384; // 16КБ
 
-  constructor(
-    private readonly filename: string
-  ) { }
-
-  public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: 'utf-8' });
+export class TSVFileReader extends EventEmitter implements FileReader {
+  constructor(private readonly filename: string) {
+    super();
   }
 
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: CHUNK_SIZE, // размер буфера
+      encoding: 'utf-8'
+    });
+
+    let remainingData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
+    }
+    this.emit('end', importedRowCount);
+  }
+
+  /*
   public toArray(): RentalOffer[] {
     if (!this.rawData) {
       throw new Error('File was not read');
@@ -47,6 +69,7 @@ export class TSVFileReader implements FileReader {
         coordinates: { latitude, longitude }
       }));
   }
+  */
 }
 
 
