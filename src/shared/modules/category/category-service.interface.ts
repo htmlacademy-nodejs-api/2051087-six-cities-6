c2310537e1064b4cd1484fd259a4cde908181597
Nikita <tereshkonikita@mail.ru>
2024-03-24 12:34:25 +0300

    add entity Category and model

---
+++ src/shared/modules/category/category-service.interface.ts
@@ -0,0 +1,10 @@
+import { DocumentType } from '@typegoose/typegoose';
+import { CreateCategoryDto } from './index.js';
+import { CategoryEntity } from './index.js';
+
+export interface CategoryService {
+  create(dto: CreateCategoryDto): Promise<DocumentType<CategoryEntity>>;
+  findByCategoryId(categoryId: string): Promise<DocumentType<CategoryEntity>> | null;
+  findByCategoryName(categoryName: string): Promise<DocumentType<CategoryEntity>> | null;
+  findByCategoryNameOrCreate(categoryName: string, dto: CreateCategoryDto): Promise<DocumentType<CategoryEntity>>;
+}
